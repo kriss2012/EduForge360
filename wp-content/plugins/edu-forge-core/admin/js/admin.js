fee64c9eb4d5855c128d44f47b61339ea74598f9
Krishna Chandrakant Patil <krishnacpatil2006@gmail.com>
2026-09-15 16:51:17 +0530

    Create admin.js

---
+++ wp-content/plugins/edu-forge-core/admin/js/admin.js
@@ -0,0 +1,12 @@
+/**
+ * EduForge Admin Scripts
+ */
+(function($) {
+	'use strict';
+
+	$(document).ready(function() {
+		// Log admin initialization
+		console.log('EduForge360 Admin Suite Loaded.');
+	});
+
+})(jQuery);
